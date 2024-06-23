2ec90a2c89626547fa53dba6d27ba1432ec51446
Tejas Nanoti <tejasnanoti2@gmail.com>
2024-06-23 20:24:31 +0530

    feat: Complete Endpoint for generating daily news summary;

--- server/controllers/newsControllers.js
+++ server/controllers/newsControllers.js
@@ -1,20 +1,25 @@
 import axios from 'axios'
-import fs from 'fs'
 import newsData from '../data/daily.json' assert { type: 'json' };
 import { JSDOM } from 'jsdom'
 import { Readability } from '@mozilla/readability'
 import { getNewsModel } from '../utils/initializeModel.js';
+import User from '../models/userModel.js';
 
 export const generateDailyNews = async (req, res) => {
     try {
+        const { userId, category } = req.body
+
+        const potentialUser = await User.findById(userId)
+
         // console.log(process.env.NEWS_API_KEY)
-        // const topHeadlines = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&pageSize=6&apiKey=${process.env.NEWS_API_KEY}`)
+        const topHeadlines = await axios.get(`https://newsapi.org/v2/top-headlines?country=${potentialUser.country}&pageSize=5&category=${category}&apiKey=${process.env.NEWS_API_KEY}`)
+        console.log(topHeadlines.data.articles)
 
         // const url = `https://newsapi.org/v2/top-headlines?country=us&pageSize=6&apiKey=${process.env.NEWS_API_KEY}`
 
         const model = getNewsModel()
         
-        const articles = await Promise.all(newsData.articles.map(async article => {
+        const articles = await Promise.all(topHeadlines.data.articles.map(async article => {
             console.log(article.author);
             
             try {
@@ -44,14 +49,28 @@ export const generateDailyNews = async (req, res) => {
         const prompt = JSON.stringify(articles)
 
         const result = await model.generateContent(prompt);
-        const response = await result.response;
+        const response = result.response;
         const text = response.text();
+
+        console.log(text);
 
         const summaries = JSON.parse(text);
 
+        const transformedSummaries = summaries.map((summary, index) => {
+
+            const { source, urlToImage, publishedAt } = topHeadlines.data.articles[index]
+
+            return {
+                ...summary,
+                source,
+                urlToImage,
+                publishedAt
+            }
+        })
+
         res
             .status(200)
-            .json({ summaries })
+            .json({ transformedSummaries })
             // .send("done")
         
     } catch (error) {
