457ce25deb768a203a7ebfcbb289546831806425
Tejas <tejasnanoti2@gmail.com>
2024-07-06 23:18:02 +0530

    feat: Add Limelight endpoint and parameter yield model;

--- server/controllers/newsControllers.js
+++ server/controllers/newsControllers.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { JSDOM } from 'jsdom'
 import { Readability } from '@mozilla/readability'
-import { getNewsModel } from '../utils/initializeModel.js';
+import { getNewsModel, getParameterYieldModel } from '../utils/initializeModel.js';
 
 export const generateDailyNews = async (req, res) => {
     try {
@@ -60,6 +60,46 @@ export const generateDailyNews = async (req, res) => {
             .status(200)
             .json({ transformedSummaries })
         
+    } catch (error) {
+        console.log(error);
+        res
+            .status(500)
+            .json({ message: "Internal server error" })
+    }
+}
+
+export const getLimelightResponse = async (req, res) => {
+    try {
+        const { prompt } = req.body;
+        const model = getParameterYieldModel()
+
+        const result = await model.generateContent(prompt);
+        const response = await result.response;
+        const text = response.text();
+
+        const parameters = JSON.parse(text);
+
+        const newsResponse = await axios.get(`https://newsapi.org/v2/everything?q=${parameters.q}&from=${parameters.from}&to=${parameters.to}&apiKey=${process.env.NEWS_API_KEY}`)
+        const { data: articles } = newsResponse
+
+        const fullArticles = await Promise.all(articles.articles.map(async article => {
+            const response = await axios.get(article.url);
+            let dom = new JSDOM(response.data, {
+                url: article.url
+            });
+            let parsedArticle = new Readability(dom.window.document).parse();
+            
+            if (parsedArticle && parsedArticle.textContent) {
+                return parsedArticle.textContent;
+            } else {
+                console.error(`Failed to parse article at URL: ${article.url}`);
+                return null;
+            }
+        }));
+
+        res
+            .status(200)
+            .json({ fullArticles })
     } catch (error) {
         console.log(error);
         res
