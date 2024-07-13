305b60a202313427a9e3969f9d68b5dd01773d04
Tejas <tejasnanoti2@gmail.com>
2024-07-13 16:10:46 +0530

    feat: Build chat interface;

--- client/app/dashboard/limelight/page.tsx
+++ client/app/dashboard/limelight/page.tsx
@@ -1,13 +1,17 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
-import { ArrowRight, SendHorizonal } from "lucide-react"
+import { ArrowRight, Clipboard, RefreshCcw, SendHorizonal } from "lucide-react"
+import chat from '@/data/chat.json'
+import CustomTooltip from "@/components/CustomTooltip"
 
-const page = () => {
+const Limelight = () => {
   return (
     <div>
-      <div className="mt-20">
-        {/* TITLE */}
+      {/* <div className="mt-20">
+        TITLE
         <section className="text-center">
           <p className="text-4xl">
             Introducing 
@@ -16,7 +20,7 @@ const page = () => {
           <p className="text-neutral-700">A chatbot to get any news you want.</p>
         </section>
 
-        {/* INPUT EXAMPLES */}
+        INPUT EXAMPLES
         <section className="grid grid-cols-2 grid-rows-2 gap-3 md:w-2/3 lg:w-1/2 mx-auto mt-8">
           <Card className="bg-red-100 hover:bg-red-200 hover:cursor-pointer">
             <CardContent className="pt-4 flex flex-col gap-1">
@@ -43,10 +47,49 @@ const page = () => {
             </CardContent>
           </Card>
         </section>
+      </div> */}
+
+
+      {/* CHAT INTERFACE */}
+      <div className="mb-12">
+        {chat.map(item => (
+            <>
+              <div className="bg-neutral-100 p-3 ml-auto rounded-md w-1/2 m-2">
+                {item.prompt}
+
+                <div className="flex mt-2">
+                  <CustomTooltip label="Copy">
+                    <Button variant="outline" size='sm' className="bg-neutral-100 hover:bg-neutral-200">
+                      <Clipboard size={16} />
+                    </Button>
+                  </CustomTooltip>
+                </div>
+              </div>
+
+              <div className="bg-neutral-100 p-3 mr-auto rounded-md w-2/3 m-2">
+                {item.response}
+
+                <div className="flex mt-2 gap-2">
+                  <CustomTooltip label="Copy">
+                    <Button variant="outline" size='sm' className="bg-neutral-100 hover:bg-neutral-200">
+                      <Clipboard size={16} />
+                    </Button>
+                  </CustomTooltip>
+                  
+                  <CustomTooltip label="Regenerate">
+                    <Button variant="outline" size='sm' className="bg-neutral-100 hover:bg-neutral-200">
+                      <RefreshCcw size={16} />
+                    </Button>
+                  </CustomTooltip>
+                </div>
+              </div>
+            </>
+          ))
+        }
       </div>
 
       {/* INPUT */}
-      <div className="flex gap-3 mt-2">
+      <div className="flex gap-3 fixed bottom-2 right-2 left-2 sm:left-[308px]">
         <Input placeholder="Chat with Limelight" />
         <Button variant='outline'><SendHorizonal size={18} /></Button>
       </div>
@@ -54,4 +97,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Limelight
