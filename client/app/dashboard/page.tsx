081e66100337353e2488ef2840a7cc56f822dfd8
Tejas <tejasnanoti2@gmail.com>
2024-07-28 22:15:49 +0530

    feat: Add User stats to dashboard;

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,8 +1,48 @@
-import UnderDevelopment from '@/components/UnderDevelopment'
+"use client"
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { useAuth } from '@/context/AuthContext'
+import CountUp from 'react-countup'
 
 const Dashboard = () => {
+
+  const { user } = useAuth()
+
   return (
-    <UnderDevelopment />
+    <main>
+      <div className='flex gap-2'>
+        <Card className='w-1/3 bg-yellow-100 text-neutral-700'>
+          <CardHeader>
+            <CardTitle>Limelight requests</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <h1>
+              <CountUp end={user?.limelightRequests as number} duration={4} />
+            </h1>
+          </CardContent>
+        </Card>
+        <Card className='w-1/3 bg-red-100 text-neutral-700'>
+          <CardHeader>
+            <CardTitle>Daily requests</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <h1>
+              <CountUp end={user?.dailySummariesRequests as number} duration={4} />
+            </h1>
+          </CardContent>
+        </Card>
+        <Card className='w-1/3 bg-blue-100 text-neutral-700'>
+          <CardHeader>
+            <CardTitle>Total API usage</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <h1>
+              <CountUp end={user?.dailySummariesRequests as number + user?.limelightRequests! as number} duration={4} />
+            </h1>
+          </CardContent>
+        </Card>
+      </div>
+    </main>
   )
 }
 
