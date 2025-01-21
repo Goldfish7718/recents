bb6a8feb9bbdbc773c18303263c1a37bffb4cec9
Tejas <tejasnanoti2@gmail.com>
2025-01-21 19:16:49 +0530

    chore: Add self-ping to server;

--- server/index.js
+++ server/index.js
@@ -1,83 +1,101 @@
 // MODULE IMPORTS
-import express from 'express'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import { config } from 'dotenv'
-import { Server } from 'socket.io'
-import http from 'http'
-import cron from 'node-cron'
-import User from './models/userModel.js';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { config } from "dotenv";
+import { Server } from "socket.io";
+import http from "http";
+import cron from "node-cron";
+import User from "./models/userModel.js";
 
-config()
+config();
 
 // ROUTE IMPORTS
-import userRoutes from './routes/userRoutes.js'
-import newsRoutes from './routes/newsRoutes.js'
-import statRoutes from './routes/statRoutes.js'
+import userRoutes from "./routes/userRoutes.js";
+import newsRoutes from "./routes/newsRoutes.js";
+import statRoutes from "./routes/statRoutes.js";
 
 // SOCKET FUNCTION IMPORTS
-import { getLimelightResponse } from './controllers/socketControllers.js'
+import { getLimelightResponse } from "./controllers/socketControllers.js";
 
-
-const app = express()
-const PORT = 5000
-const server = http.createServer(app)
+const app = express();
+const PORT = 5000;
+const server = http.createServer(app);
 
 if (process.env.ORIGIN) {
-    app.use(cors({
-        credentials: true,
-        origin: process.env.ORIGIN
-    }))
+  app.use(
+    cors({
+      credentials: true,
+      origin: process.env.ORIGIN,
+    })
+  );
 } else {
-    app.use(cors())
+  app.use(cors());
 }
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // ROUTES MIDDLEWARE
-app.use('/user', userRoutes)
-app.use('/news', newsRoutes)
-app.use('/stats', statRoutes)
+app.use("/user", userRoutes);
+app.use("/news", newsRoutes);
+app.use("/stats", statRoutes);
 
-app.get('/', (req, res) => {
-    res.send("Recents Server")
-})
-
-const io = new Server(server, {
-    cors: {
-      origin: process.env.ORIGIN,
-      methods: ["GET", "POST"],
-    },
+app.get("/", (req, res) => {
+  res.send("Recents Server");
 });
 
-io.on('connection', (socket) => {
-    console.log(`User Logged: ${socket.id}`);
+const io = new Server(server, {
+  cors: {
+    origin: process.env.ORIGIN,
+    methods: ["GET", "POST"],
+  },
+});
 
-    socket.on('get_response', async data => {
-        const res = await getLimelightResponse(data.prompt, data.clerkId, socket)
-        socket.emit('receive', res)
-    })
-})
+io.on("connection", (socket) => {
+  console.log(`User Logged: ${socket.id}`);
 
-cron.schedule('0 0 0 * * *', async () => {
+  socket.on("get_response", async (data) => {
+    const res = await getLimelightResponse(data.prompt, data.clerkId, socket);
+    socket.emit("receive", res);
+  });
+});
+
+cron.schedule(
+  "0 0 0 * * *",
+  async () => {
     try {
-      await User.updateMany({}, { $set: { limelightRequests: 0, dailySummariesRequests: 0 } });
-      console.log('All users\' requests have been reset to 0');
+      await User.updateMany(
+        {},
+        { $set: { limelightRequests: 0, dailySummariesRequests: 0 } }
+      );
+      console.log("All users' requests have been reset to 0");
     } catch (error) {
-      console.error('Error resetting user requests:', error);
+      console.error("Error resetting user requests:", error);
     }
-}, {timezone:'Asia/Calcutta'});
+  },
+  { timezone: "Asia/Calcutta" }
+);
 
-
+cron.schedule(
+  "*/14 * * * *",
+  async () => {
+    try {
+      await fetch(process.env.SERVER_URL);
+    } catch (error) {
+      console.error("Error pinging server", error);
+    }
+  },
+  { timezone: "Asia/Calcutta" }
+);
 
 const connectDB = async (url) => {
-    await mongoose
-        .connect(url)
-        .then(() => console.log("Database Connected"))
-        .catch(err => console.log(err))
-}
+  await mongoose
+    .connect(url)
+    .then(() => console.log("Database Connected"))
+    .catch((err) => console.log(err));
+};
 
 server.listen(PORT, async () => {
-    await connectDB(process.env.DB_URI || 'mongodb://0.0.0.0:27017/recents-DB')
-    console.log(`Server started on port ${PORT}`);    
-})
+  await connectDB(process.env.DB_URI || "mongodb://0.0.0.0:27017/recents-DB");
+  console.log(`Server started on port ${PORT}`);
+});
