667977592ec9661bbd3ada6e86ad8482b2d5b108
Tejas Nanoti <tejasnanoti2@gmail.com>
2025-02-09 11:04:50 -0500

    chore: Remove unused import; First commit from Fedora Linux!

--- client/app/dashboard/layout.tsx
+++ client/app/dashboard/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import ProtectRoute from "@/components/ProtectRoute";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { SignOutButton } from "@clerk/nextjs";
